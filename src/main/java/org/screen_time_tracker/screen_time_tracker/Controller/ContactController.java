731fe5e41d2007ba6e8019ca29b621c0db2c5903
PixelSTCK <yuhsienwu34@gmail.com>
2024-05-13 12:40:11 +1000

    Removed TextField in ContactController

--- src/main/java/org/screen_time_tracker/screen_time_tracker/Controller/ContactController.java
+++ src/main/java/org/screen_time_tracker/screen_time_tracker/Controller/ContactController.java
@@ -7,7 +7,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import org.screen_time_tracker.screen_time_tracker.MainApplication;
